98b2de6f0fd99a7782626a96103ff7482bb8b26a
NATroutter <natroutter@gmail.com>
2025-02-07 17:03:33 +0200

    version update for testing XD

--- src/main/java/fi/natroutter/foxlib/FoxLib.java
+++ src/main/java/fi/natroutter/foxlib/FoxLib.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Getter
 public class FoxLib {
 
-    public String Version = "1.2.1";
+    public String Version = "1.2.2";
 
     public static void print(Object message) {
         System.out.print(message.toString());
