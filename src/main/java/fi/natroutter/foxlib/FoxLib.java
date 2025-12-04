bdcf1a69b66bbf6a804c39b0ae4e73b73efe1d6e
NATroutter <natroutter@gmail.com>
2025-12-04 20:11:22 +0200

    test ci

--- src/main/java/fi/natroutter/foxlib/FoxLib.java
+++ src/main/java/fi/natroutter/foxlib/FoxLib.java
@@ -17,7 +17,7 @@ import java.util.concurrent.ThreadLocalRandom;
 @Getter
 public class FoxLib {
 
-    public String Version = "1.3.6";
+    public String Version = "1.3.6";//
 
     public static void print(Object message) {
         System.out.print(message.toString());
